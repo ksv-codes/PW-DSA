21909510ac4113579c9938023acd6f4498a40f33
Keshav Sharma <sharmakeshav2603@gmail.com>
2024-06-21 19:34:10 +0530

    Add files via upload

---
+++ Array/basic.java
@@ -0,0 +1,8 @@
+public class basic{
+    public static void main(String[] args) {
+        int x = 5;
+        int[] arr = new int[5];
+        System.out.println(arr[0]);
+        System.out.println(x);
+    }
+}
